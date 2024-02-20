bc3ca00c5a53ae8bb5667647ec4a8219fa08c0a7
raphavtorres <raphaeltorresprofissional@gmail.com>
2024-02-20 14:15:39 -0300

    feat: error handling

---
+++ src/main/java/com/api/delivery/exception/ErrorHandler.java
@@ -0,0 +1,32 @@
+package com.api.delivery.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handleError404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleError(MethodArgumentNotValidException exception) {
+        var errors = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(ErrorValidationDto::new).toList());
+    }
+
+    // MY CUSTOM VALIDATIONS
+
+    public record ErrorValidationDto(String field, String message) {
+        public ErrorValidationDto(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+
+}
---
+++ src/main/java/com/api/delivery/exception/ValidationException.java
@@ -0,0 +1,8 @@
+package com.api.delivery.exception;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+}
